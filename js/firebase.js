7fd6bb7ab1a2e5c9ca29764a24fa344107615f04
modxcrazy <in99soft.dev.pvt.ltd@gmail.com>
2025-04-22 08:14:17 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyBWc2uI9P_r1O91umKBs_2HwZRqZjW40v8",
+    authDomain: "j4jegamer-efd7b.firebaseapp.com",
+    databaseURL: "https://j4jegamer-efd7b-default-rtdb.firebaseio.com",
+    projectId: "j4jegamer-efd7b",
+    storageBucket: "j4jegamer-efd7b.firebasestorage.app",
+    messagingSenderId: "486568531489",
+    appId: "1:486568531489:web:6ef6ceb0b870fdb908b560",
+    };
+    
+    firebase.initializeApp(firebaseConfig);
